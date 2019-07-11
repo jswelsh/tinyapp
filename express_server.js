a5549e44634b386332feb75786512e3832c57ddc
jswelsh <james.sinclair.welsh@gmail.com>
2019-07-11 02:55:44 +0000

    added the login function and database

--- express_server.js
+++ express_server.js
@@ -14,6 +14,19 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const users = { 
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+}
+
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL.substring(1)];
   console.log(longURL);
@@ -23,19 +36,38 @@ app.get("/u/:shortURL", (req, res) => {
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
+app.post("/urls", (req, res) => {
+  res.redirect('/urls');
+})
+
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 app.post("/urls/newmake", (req, res) => {
-  const hold = generateRandomString()
-  req.body[Object.keys(req.body)[0]];
-  urlDatabase[hold] = req.body[Object.keys(req.body)[0]];
-  console.log(urlDatabase);
+  const id = generateRandomString()
+  urlDatabase[id] = req.body.longURL;
+/*   req.body[Object.keys(req.body)[0]];
+  urlDatabase[id] = req.body[Object.keys(req.body)[0]];
+  console.log(urlDatabase); */
   res.redirect(`/urls`);
 });
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  res.render("urls_new"); 
+});
+app.post("/register", (req, res) => {
+  console.log(req.body);
+  console.log(req.body.email);
+  const id = generateRandomString()
+  users[id] = { 
+    id: id, 
+    email: req.body.email,
+    password: req.body.password
+  }
+  res.redirect('/urls');
+})
+app.get("/register", (req, res) => {
+  res.render("urls_register");
 });
 app.post("/urls/:shortURL/delete", (req, res) => {
   delete urlDatabase[req.params.shortURL];
